5acf97a8fd7db0fd65c5506a90f7c0a9c71850ed
OmerB <25951112+OmerB@users.noreply.github.com>
2023-01-20 19:23:54 +0300

    Business katmanı güncellendi

---
+++ Business/Abstract/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using Entities.DTOs.UserDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserDetailDto>> GetAll();
+        Task<UserDto> Get(int id);
+        Task<UserDto> Add(UserAddDto UserAddDto);
+        Task<bool> Delete(int id);
+        Task<UserUpdateDto> Update(UserUpdateDto UserUpdateDto);
+    }
+}
